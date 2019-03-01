030e9d4ce4a65217f1a413f3f56856ee0ffcae5a
Vincent Leombruno <vleombruno42@gmail.com>
2019-02-28 21:41:13 -0500

    Added Wound Triggers & AutoWounds

--- src/__tests__/calculations/WoundCalculator.test.js
+++ src/__tests__/calculations/WoundCalculator.test.js
@@ -1,4 +1,4 @@
-import WoundCalculator, { toughnesses, neededRollToWound } from 'calculations/WoundCalculator';
+import WoundCalculator, { toughnesses, AutoWound, neededRollToWound } from 'calculations/WoundCalculator';
 import { RerollType } from 'calculations/Dice';
 
 import { Chance } from 'chance';
@@ -21,6 +21,15 @@ describe('neededRollToWound', () => {
     expect(neededRollToWound(4, 5)).toEqual(5);
     expect(neededRollToWound(4, 8)).toEqual(6);
     expect(neededRollToWound(4, 9)).toEqual(6);
+  });
+});
+
+describe('AutoWound', () => {
+  it('is an object to help define various auto-wound types', () => {
+    expect(typeof AutoWound).toBe('object');
+    expect(AutoWound.ALWAYS).toEqual('ALWAYS');
+    expect(AutoWound.TOUGHNESS_GT_STRENGTH).toEqual('TOUGHNESS_GT_STRENGTH');
+    expect(AutoWound.NONE).toEqual('NONE');
   });
 });
 
@@ -53,6 +62,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.NONE)
         .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator.getUnmodifiedWoundsOn()).toEqual([3, 4, 5, 5, 5, 6]);
     });
@@ -66,6 +77,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.NONE)
         .withWoundModifier(-1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getModifiedWoundsOn()).toEqual([4, 5, 6, 6, 6, 7]);
 
@@ -74,6 +87,8 @@ describe('WoundCalculator', () => {
         .withStrength(6)
         .withRerollOn(RerollType.NONE)
         .withWoundModifier(3)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build(); // Cannot go below 2
       expect(woundCalculator2.getModifiedWoundsOn()).toEqual([2, 2, 2, 2, 2, 2]);
     });
@@ -87,6 +102,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.NONE)
         .withWoundModifier(-1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getWoundChances()).toEqual([3/6, 2/6, 1/6, 1/6, 1/6, 0]);
     });
@@ -100,6 +117,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.NONE)
         .withWoundModifier(-1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getRawWounds()).toEqual([3, 2, 1, 1, 1, 0]);
     });
@@ -113,6 +132,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.ONES)
         .withWoundModifier(-1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getModifiedRerollMaxValues()).toEqual([1, 1, 1, 1, 1, 1]);
     });
@@ -123,6 +144,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.NONE)
         .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getModifiedRerollMaxValues()).toEqual([0, 0, 0, 0, 0, 0]);
     });
@@ -133,6 +156,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.ALL)
         .withWoundModifier(-1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getModifiedRerollMaxValues()).toEqual([2, 3, 4, 4, 4, 5]);
     });
@@ -143,6 +168,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.ALL)
         .withWoundModifier(2)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getModifiedRerollMaxValues()).toEqual([1, 1, 2, 2, 2, 3]);
     });
@@ -153,6 +180,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn('WAT')
         .withWoundModifier(2)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(() => woundCalculator1.getModifiedRerollMaxValues()).toThrow('Unknown RerollType, WAT');
     });
@@ -166,6 +195,8 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.ALL)
         .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator1.getRerollWounds()).toBeDeepCloseTo([1.33, 1.5, 1.33, 1.33, 1.33, 0.83], 2);
 
@@ -174,12 +205,121 @@ describe('WoundCalculator', () => {
         .withStrength(6)
         .withRerollOn(RerollType.ALL)
         .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
         .build();
       expect(woundCalculator2.getRerollWounds()).toBeDeepCloseTo([0.83, 1.33, 1.33, 1.5, 1.33, 1.33], 2);
     });
   });
 
-  describe('Get Total Wounds', () => {
+  describe('Get Total Rolls', () => {
+
+    it('gets the total wound roll attempts made', () => {
+      const woundCalculator1 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(4)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .build();
+      expect(woundCalculator1.getTotalWoundRolls()).toBeDeepCloseTo([8, 9, 10, 10, 10, 11], 2);
+
+      const woundCalculator2 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(5)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .build();
+      expect(woundCalculator2.getTotalWoundRolls()).toBeDeepCloseTo([7, 7, 8, 9, 9, 9], 2);
+
+      const woundCalculator3 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(4)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(-1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .build();
+      expect(woundCalculator3.getTotalWoundRolls()).toBeDeepCloseTo([8, 9, 10, 10, 10, 11], 2);
+    });
+  });
+
+  describe('Get Modified Trigger Value', () => {
+    it('modifies the required minimum trigger value', () => {
+      const woundCalculator1 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(4)
+        .withRerollOn(RerollType.NONE)
+        .withWoundModifier(1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(4)
+        .withExtraMortalsOnTrigger(0)
+        .build();
+      expect(woundCalculator1.getModifiedTriggerValue()).toEqual(3);
+    });
+  });
+
+  describe('Get Total Triggers', () => {
+    it('gets the total triggers that occured during all rolling', () => {
+      const woundCalculator1 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(4)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(4)
+        .withExtraMortalsOnTrigger(1)
+        .build();
+      expect(woundCalculator1.getTotalTriggers()).toEqual([4, 4.5, 5, 5, 5, 5.5]);
+
+      const woundCalculator2 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(5)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(5)
+        .withExtraMortalsOnTrigger(1)
+        .build();
+      expect(woundCalculator2.getTotalTriggers()).toEqual([3.5, 3.5, 4, 4.5, 4.5, 4.5]);
+
+      const woundCalculator3 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(1)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(-1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(5)
+        .withExtraMortalsOnTrigger(1)
+        .build();
+      expect(woundCalculator3.getTotalTriggers()).toBeDeepCloseTo([1.83, 1.83, 1.83, 1.83, 1.83, 1.83], 2);
+    });
+  });
+
+  describe('Get Total Mortal Wounds', () => {
+    it('gets the total mortal wounds', () => {
+      const woundCalculator2 = new WoundCalculator.Builder()
+        .withHits(6)
+        .withStrength(4)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(4)
+        .withExtraMortalsOnTrigger(2)
+        .build();
+      expect(woundCalculator2.getTotalMortalWounds()).toEqual([8, 9, 10, 10, 10, 11]);
+    });
+  });
+
+  describe('Get Successful Wound Rolls', () => {
 
     it('gets the total wounds from the calculator', () => {
       const woundCalculator1 = new WoundCalculator.Builder()
@@ -187,40 +327,77 @@ describe('WoundCalculator', () => {
         .withStrength(4)
         .withRerollOn(RerollType.NONE)
         .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(0)
+        .withExtraMortalsOnTrigger(0)
         .build();
-      expect(woundCalculator1.getTotalWounds()).toBeDeepCloseTo([4, 3, 2, 2, 2, 1], 2);
+      expect(woundCalculator1.getTotalNormalWounds()).toBeDeepCloseTo([4, 3, 2, 2, 2, 1], 2);
 
       const woundCalculator2 = new WoundCalculator.Builder()
         .withHits(6)
         .withStrength(4)
         .withRerollOn(RerollType.ONES)
         .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(0)
+        .withExtraMortalsOnTrigger(0)
         .build();
-      expect(woundCalculator2.getTotalWounds()).toBeDeepCloseTo([4.66, 3.5, 2.33, 2.33, 2.33, 1.17], 2);
+      expect(woundCalculator2.getTotalNormalWounds()).toBeDeepCloseTo([4.66, 3.5, 2.33, 2.33, 2.33, 1.17], 2);
 
       const woundCalculator3 = new WoundCalculator.Builder()
         .withHits(6)
         .withStrength(4)
         .withRerollOn(RerollType.ALL)
         .withWoundModifier(0)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(0)
+        .withExtraMortalsOnTrigger(0)
         .build();
-      expect(woundCalculator3.getTotalWounds()).toBeDeepCloseTo([5.33, 4.5, 3.33, 3.33, 3.33, 1.83], 2);
+      expect(woundCalculator3.getTotalNormalWounds()).toBeDeepCloseTo([5.33, 4.5, 3.33, 3.33, 3.33, 1.83], 2);
 
       const woundCalculator4 = new WoundCalculator.Builder()
         .withHits(6)
         .withStrength(4)
         .withRerollOn(RerollType.ALL)
         .withWoundModifier(-4)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(0)
+        .withExtraMortalsOnTrigger(0)
         .build();
-      expect(woundCalculator4.getTotalWounds()).toBeDeepCloseTo([0, 0, 0, 0, 0, 0], 2);
+      expect(woundCalculator4.getTotalNormalWounds()).toBeDeepCloseTo([0, 0, 0, 0, 0, 0], 2);
 
       const woundCalculator5 = new WoundCalculator.Builder()
         .withHits(6)
         .withStrength(7)
         .withRerollOn(RerollType.ALL)
         .withWoundModifier(1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(0)
+        .withExtraMortalsOnTrigger(0)
         .build();
-      expect(woundCalculator5.getTotalWounds()).toBeDeepCloseTo([5.83, 5.83, 5.83, 5.83, 5.33, 4.5], 2);
+      expect(woundCalculator5.getTotalNormalWounds()).toBeDeepCloseTo([5.83, 5.83, 5.83, 5.83, 5.33, 4.5], 2);
+
+      const woundCalculator6 = new WoundCalculator.Builder()
+        .withHits(12)
+        .withStrength(4)
+        .withRerollOn(RerollType.ALL)
+        .withWoundModifier(1)
+        .withAutoWoundCondition(AutoWound.NONE)
+        .withAutoWoundOn(0)
+        .withMinTriggerValue(5)
+        .withExtraMortalsOnTrigger(0)
+        .withApModifierOnTrigger(1)
+        .withDamageReplacementOnTrigger(0)
+        .build();
+      expect(woundCalculator6.getTotalModifiedWounds()).toBeDeepCloseTo([7, 8, 9, 9, 9, 6.66], 2);
+      expect(woundCalculator6.getTotalNormalWounds()).toBeDeepCloseTo([4.66, 2.66, 0, 0, 0, 0], 2);
+
+      // TODO: Add more tests as shit eventually maybe breaks
     });
   });
 
